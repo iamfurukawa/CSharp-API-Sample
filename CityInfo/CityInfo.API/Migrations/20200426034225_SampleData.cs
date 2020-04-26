4a204649adabcb8d720f29fab6195242ae1e802a
Vinícius Furukawa Carvalho <vinicius_furuka@hotmail.com.br>
2020-04-26 00:43:47 -0300

    Ending of Part 6.

---
+++ CityInfo/CityInfo.API/Migrations/20200426034225_SampleData.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CityInfo.API.Migrations
+{
+    public partial class SampleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "description",
+                table: "PointOfInterests",
+                newName: "Description");
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "The one with that big park.", "New York City" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 2, "The one with the cathedral that was never really finished.", "Antwerp" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 3, "The one with that big tower.", "Paris" });
+
+            migrationBuilder.InsertData(
+                table: "PointOfInterests",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "The most visited urban park in the US.", "Central Park" },
+                    { 2, 1, "A 102-story skycraper located in Midtown Manhattan.", "Empire State Building" },
+                    { 3, 2, "A Gothic style cathedral.", "Cathedral of Our Lady" },
+                    { 4, 2, "The finest example of railway archtecture in Belgium.", "Antwerp Central Station" },
+                    { 5, 3, "A wrought iron lattice tower on the Champ de Mars.", "Effiel Tower" },
+                    { 6, 3, "The world`s lasgest museum.", "The Louvre" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PointOfInterests",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "PointOfInterests",
+                newName: "description");
+        }
+    }
+}
